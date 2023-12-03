5f2a195e5e51ac4c82dc2ade2575ec26fc99135b
GrandMasterOzGMO <139928557+GrandMasterOzGMO@users.noreply.github.com>
2023-12-03 11:37:21 -0500

    Stars rating added along with a welcomepage image which is not implimented as yet

--- frontend/src/components/HalfRating.js
+++ frontend/src/components/HalfRating.js
@@ -1,15 +1,41 @@
+// src/components/HalfRating.js
 import * as React from 'react';
 import Rating from '@mui/material/Rating';
-import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+
+const labels = {
+  0.5: 'Useless+',
+  1: 'Useless+',
+  1.5: 'Poor+',
+  2: 'Poor+',
+  2.5: 'Ok+',
+  3: 'Ok+',
+  3.5: 'Good+',
+  4: 'Good+',
+  4.5: 'Excellent+',
+  5: 'Excellent+',
+};
 
 export default function HalfRating({ value, readOnly }) {
-    console.log('Rating value:', value);
-    console.log('Is Read Only:', readOnly);
-    
+  const [hover, setHover] = React.useState(-1);
+
   return (
-    <Stack spacing={1}>
-      <Rating name="half-rating" Value={2.5} precision={0.5} />
-      <Rating name="half-rating-read" Value={2.5} precision={0.5} readOnly />
-    </Stack>
+    <div>
+      <Rating
+        name="hover-feedback"
+        value={value}
+        precision={0.5}
+        onChange={(event, newValue) => {
+          // Handle rating change if needed
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+        emptyIcon={<span style={{ opacity: 0.55 }}>★</span>}
+      />
+      {value !== null && (
+        <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+      )}
+    </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './App'; // or the path to your main component
 
 const theme = createTheme();
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-);
+createRoot(document.getElementById('root')).render(<App />);
